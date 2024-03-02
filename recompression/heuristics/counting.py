397bdca3661f42c42f20947902c150d7213fdd92
postlog <scarfaceborn@mail.ru>
2024-03-02 13:31:17 +0300

    Fix counting heuristics

--- recompression/heuristics/counting.py
+++ recompression/heuristics/counting.py
@@ -7,41 +7,45 @@ from utils.time import timeit
 
 class CountingHeuristics(h.Heurisitcs):
     def __init__(self):
-        self.z3 = z3.Solver()
+        self._z3 = z3.Solver()
 
     @timeit("heuristic of counting consts")
     def is_satisfable(self, equation: eq.Equation, option: opt.Option) -> bool:
-        results = []
-        tpl_vars = equation.template.get_vars()
         tpl_consts = equation.template.get_consts()
-        tpl_elements = tpl_vars.union(tpl_consts)
+        lhs_vars = []
+        lhs_vars_bools = []
+        not_empty_count = 0
+        for element in equation.template.elements:
+            if not isinstance(element, v.Var):
+                continue
+            var = z3.Int(str(element))
+            lhs_vars.append(var)
+            lhs_vars_bools.append(var >= 0)
+            if option.restriction is not None and not option.restriction.is_substitution_satisfies(opt.EmptySubstitution(element)):
+                not_empty_count += 1
 
         consts = equation.sample.get_consts().union(tpl_consts)
         for const in consts:
-            self.z3.reset()
+            self._z3.reset()
 
-            lhs_count = 0
-            lhs_vars = []
-            lhs_vars_bools = []
-            for element in tpl_elements:
+            spl_count = 0
+            for element in equation.sample.elements:
                 if element == const:
-                    lhs_count += 1
-                elif isinstance(element, v.Var):
-                    var = z3.Int(str(element))
-                    lhs_vars.append(var)
-                    lhs_vars_bools.append(var >= 0)
+                    spl_count += 1
 
-            lhs_z3_obj = z3.IntVal(lhs_count)
+            tpl_count = 0
+
+            for element in equation.template.elements:
+                if element == const:
+                    tpl_count += 1
+
+            lhs_z3_obj = z3.IntVal(tpl_count)
             for var in lhs_vars:
                 lhs_z3_obj += var
 
-            rhs_count = 0
-            for element in equation.sample.elements:
-                if element == const:
-                    rhs_count += 1
+            self._z3.add(*lhs_vars_bools, lhs_z3_obj == z3.IntVal(spl_count))
+            check_result = self._z3.check()
+            if check_result not in (z3.sat, z3.unknown):
+                return False
 
-            self.z3.add(*lhs_vars_bools, lhs_z3_obj == z3.IntVal(rhs_count))
-            c = self.z3.check()
-            results.append(c in (z3.sat, z3.unknown))
-
-        return all(results)
+        return True
